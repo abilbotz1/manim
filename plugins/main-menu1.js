d930f3491504a1da61cdc5f0c6c744b7c391c888
abilbotz1 <108676663+abilbotz1@users.noreply.github.com>
2022-09-22 15:50:51 +0800

    Update main-menu1.js

--- plugins/main-menu1.js
+++ plugins/main-menu1.js
@@ -3,13 +3,13 @@ let moment = require('moment-timezone')
 let time = moment.tz('Asia/Jakarta').format("HH:mm:ss")
 let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
 
-conn.sendButtonDoc(m.chat, 'Halo, Saya ImanBot, Saya diciptakan Oleh Seseorang Manusia yang bernama Iman, Tujuan bot ini dibuat adalah sekedar hiburan dan menambah pengetahuan 😇\n\nɴᴏᴛᴇ: ʜᴀʀᴀᴘ ɢᴜɴᴀᴋᴀɴ ʙᴏᴛ ꜱᴇᴄᴀʀᴀ ʙɪᴊᴀᴋ ᴅᴀɴ ᴊᴀɴɢᴀɴ ᴅɪꜱᴘᴀᴍ', data.namabot, 'ʟɪꜱᴛᴍᴇɴᴜ', '.m', fake, { contextInfo: {
+conn.sendButtonDoc(m.chat, 'ʜᴀʟᴏ, sᴀʏᴀ ᴋʜᴜʀᴜsʜɪ-ʙᴏᴛᴢ, Saya diciptakan Oleh Seseorang Manusia yang bernama ʀɪᴇʟXᴅ, Tujuan bot ini dibuat adalah sekedar hiburan dan menambah pengetahuan 😇\n\nɴᴏᴛᴇ: ʜᴀʀᴀᴘ ɢᴜɴᴀᴋᴀɴ ʙᴏᴛ ꜱᴇᴄᴀʀᴀ ʙɪᴊᴀᴋ ᴅᴀɴ ᴊᴀɴɢᴀɴ ᴅɪꜱᴘᴀᴍ', data.namabot, '❒ ʟɪsᴛᴍᴇɴᴜ', '.m', fake, { contextInfo: {
 externalAdReply :{
     showAdAttribution: true,
     mediaUrl: data.sc,
     mediaType: 2,
     sourceUrl: data.sc,
-    title: 'Jangan Dispam Ya Kak',
+    title: 'Jangan Dispam Ya Kak | (• ◡•)|',
     body: wm,
     thumbnail: await(await fetch(img)).buffer(),
 }}})
