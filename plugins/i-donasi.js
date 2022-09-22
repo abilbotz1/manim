0b6580460a9d1d5e96bd9cd17148f6bbd5557c6b
abilbotz1 <108676663+abilbotz1@users.noreply.github.com>
2022-09-22 15:17:43 +0800

    Update i-donasi.js

--- plugins/i-donasi.js
+++ plugins/i-donasi.js
@@ -8,15 +8,15 @@ let handler = async (m, { conn, usedPrefix }) => {
       extendedTextMessage: {
       text: `
 ╭─「 Donasi • Dana 」
-│ • Triii [62895385157376]
-│ • Gopay  [62895385157376]
-│ • Dana  [62895385157376]
-│ • Qris  [Chat OWNER]
+│ • Axis [6283813417529]
+│ • Gopay  [-]
+│ • Dana  [-]
+│ • Qris  [-]
 ╰────
 ╭─「 *NOTE* 」
 │ > Ingin donasi? Wa.me/6289522724388
 │ _Hasil donasi akan digunakan buat sewa_
-│ _atau beli *RDP/VPS* agar bot bisa jalan_
+│ _atau beli *RDP/PANEL* agar bot bisa jalan_
 │ _24jam tanpa kendala_
 ╰────
 `,
